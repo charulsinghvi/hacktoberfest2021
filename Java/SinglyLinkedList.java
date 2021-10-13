acb6825ae69529f41ee236e74ff73ab6dfbf19e1
charulsinghvi <43578752+charulsinghvi@users.noreply.github.com>
2021-10-13 23:31:33 +0530

    Update SinglyLinkedList.java

--- Java/SinglyLinkedList.java
+++ Java/SinglyLinkedList.java
@@ -1,5 +1,5 @@
-package singlylinkedlist;
-public class SinglyLinkedList {       
+public class SinglyLinkedList {    
+    //Represent a node of the singly linked list    
     class Node{    
         int data;    
         Node next;    
@@ -9,26 +9,33 @@ public class SinglyLinkedList {
             this.next = null;    
         }    
     }    
-       
+     
+    //Represent the head and tail of the singly linked list    
     public Node head = null;    
     public Node tail = null;    
-  
+        
+    //addNode() will add a new node to the list    
     public void addNode(int data) {    
-    
+        //Create a new node    
         Node newNode = new Node(data);    
-          
+            
+        //Checks if the list is empty    
         if(head == null) {    
+            //If list is empty, both head and tail will point to new node    
             head = newNode;    
             tail = newNode;    
         }    
-        else {     
+        else {    
+            //newNode will be added after tail such that tail's next will point to newNode    
             tail.next = newNode;    
+            //newNode will become new tail of the list    
             tail = newNode;    
         }    
     }    
-         
+        
+    //display() will display all the nodes present in the list    
     public void display() {    
-
+        //Node current will point to head    
         Node current = head;    
             
         if(head == null) {    
@@ -36,8 +43,9 @@ public class SinglyLinkedList {
             return;    
         }    
         System.out.println("Nodes of singly linked list: ");    
-        while(current != null) {      
-            System.out.print(current.data + " -> ");    
+        while(current != null) {    
+            //Prints each node by incrementing pointer    
+            System.out.print(current.data + " ");    
             current = current.next;    
         }    
         System.out.println();    
@@ -46,16 +54,14 @@ public class SinglyLinkedList {
     public static void main(String[] args) {    
             
         SinglyLinkedList sList = new SinglyLinkedList();    
-  
-        sList.addNode(33);    
+            
+        //Add nodes to the list    
+        sList.addNode(1);    
         sList.addNode(2);    
-        sList.addNode(24);    
-        sList.addNode(99);    
-        sList.addNode(8);
-        sList.addNode(5);
-        sList.addNode(13);
-        sList.addNode(7);
-        
+        sList.addNode(3);    
+        sList.addNode(4);    
+            
+        //Displays the nodes present in the list    
         sList.display();    
     }    
 }    
